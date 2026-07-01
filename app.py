5e7c981544282d5ea249ec21ba423a876442bdd6
Amoebius1 <99347855+Amoebius1@users.noreply.github.com>
2026-07-01 10:41:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 
 import streamlit as st
-import pandas as pd
-from elo import calculate_elo
 
-st.set_page_config(page_title="FIFA WM Elo Tracker", layout="wide")
 st.title("FIFA WM Elo Tracker")
+st.success("Die App läuft.")
